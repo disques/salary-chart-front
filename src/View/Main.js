64d8250c6be07e85dcf01ccb40757d76d9644b0c
sunny6452 <tjsgml3325@naver.com>
2023-12-11 11:46:47 +0900

    react-spring 주석

--- src/View/Main.js
+++ src/View/Main.js
@@ -9,8 +9,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import { AppContext } from "../AppContext";
 import Header from "./Header";
 import MainComponent from "./Component/MainComponent";
-import MainChart from "./Pay_Chart/MainChart";
-import Pdf_Print from "./Pay_Chart/Component_Test/Pdf_Print";
+// import MainChart from "./Pay_Chart/MainChart";
+// import Pdf_Print from "./Pay_Chart/Component_Test/Pdf_Print";
 import { Box, Button, Typography, CssBaseline } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -45,8 +45,10 @@ const Main = () => {
         <main className={classes.body}>
           <Switch>
             <Route exact path="/chart" component={MainComponent} />
-            <Route exact path="/paychart" component={MainChart} />
-            <Route exact path="/test" component={Pdf_Print} />
+            {/* 사용안하는앤데 react-spring 라이브러리 에러나서 주석함 23.12.11 선희 */}
+
+            {/* <Route exact path="/paychart" component={MainChart} /> */}
+            {/* <Route exact path="/test" component={Pdf_Print} /> */}
           </Switch>
         </main>
       </Box>
